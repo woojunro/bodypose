2dd01f241361260e6549498700d6c2e60f357b93
재승 김 <ceo@fmonth.com>
2020-12-30 18:39:45 +0900

    balancebutton picture upload

---
+++ src/virtualDB/items/DbPhotos.js
@@ -0,0 +1,719 @@
+import aavec1 from '../images/AAVEC1.jpg';
+import aavec2 from '../images/AAVEC2.jpg';
+import aavec3 from '../images/AAVEC3.jpg';
+import aavec4 from '../images/AAVEC4.jpg';
+import aavec5 from '../images/AAVEC5.jpg';
+import aavec6 from '../images/AAVEC6.jpg';
+import aavec7 from '../images/AAVEC7.jpg';
+import aavec8 from '../images/AAVEC8.jpg';
+import aavec9 from '../images/AAVEC9.jpg';
+import aavec10 from '../images/AAVEC10.jpg';
+import aavec11 from '../images/AAVEC11.jpg';
+import aavec12 from '../images/AAVEC12.jpg';
+import aavec13 from '../images/AAVEC13.jpg';
+import aavec14 from '../images/AAVEC14.jpg';
+import aavec15 from '../images/AAVEC15.jpg';
+import aavec16 from '../images/AAVEC16.jpg';
+import aavec17 from '../images/AAVEC17.jpg';
+import aavec18 from '../images/AAVEC18.jpg';
+import aavec19 from '../images/AAVEC19.jpg';
+import aavec20 from '../images/AAVEC20.jpg';
+import aavec21 from '../images/AAVEC21.jpg';
+import aavec22 from '../images/AAVEC22.jpg';
+import aavec23 from '../images/AAVEC23.jpg';
+import aavec24 from '../images/AAVEC24.jpg';
+import aavec25 from '../images/AAVEC25.jpg';
+import aavec26 from '../images/AAVEC26.jpg';
+import aavec27 from '../images/AAVEC27.jpg';
+import aavec28 from '../images/AAVEC28.jpg';
+import aavec29 from '../images/AAVEC29.jpg';
+import aavec30 from '../images/AAVEC30.jpg';
+import aavec31 from '../images/AAVEC31.jpg';
+import aavec32 from '../images/AAVEC32.jpg';
+import balanceButton1 from '../images/Balance Button1.jpg';
+import balanceButton2 from '../images/Balance Button2.jpg';
+import balanceButton3 from '../images/Balance Button3.jpg';
+import balanceButton4 from '../images/Balance Button4.jpg';
+import balanceButton5 from '../images/Balance Button5.jpg';
+import balanceButton6 from '../images/Balance Button6.jpg';
+import balanceButton7 from '../images/Balance Button7.jpg';
+import balanceButton8 from '../images/Balance Button8.jpg';
+import balanceButton9 from '../images/Balance Button9.jpg';
+import balanceButton10 from '../images/Balance Button10.jpg';
+import balanceButton11 from '../images/Balance Button11.jpg';
+import balanceButton12 from '../images/Balance Button12.jpg';
+import balanceButton13 from '../images/Balance Button13.jpg';
+import balanceButton14 from '../images/Balance Button14.jpg';
+import balanceButton15 from '../images/Balance Button15.jpg';
+import balanceButton16 from '../images/Balance Button16.jpg';
+import balanceButton17 from '../images/Balance Button17.jpg';
+import balanceButton18 from '../images/Balance Button18.jpg';
+import balanceButton19 from '../images/Balance Button19.jpg';
+import balanceButton20 from '../images/Balance Button20.jpg';
+import balanceButton21 from '../images/Balance Button21.jpg';
+import balanceButton22 from '../images/Balance Button22.jpg';
+import balanceButton23 from '../images/Balance Button23.jpg';
+import balanceButton24 from '../images/Balance Button24.jpg';
+import balanceButton25 from '../images/Balance Button25.jpg';
+import balanceButton26 from '../images/Balance Button26.jpg';
+import balanceButton27 from '../images/Balance Button27.jpg';
+import balanceButton28 from '../images/Balance Button28.jpg';
+import balanceButton29 from '../images/Balance Button29.jpg';
+import balanceButton30 from '../images/Balance Button30.jpg';
+import balanceButton31 from '../images/Balance Button31.jpg';
+import balanceButton32 from '../images/Balance Button32.jpg';
+import coco1 from '../images/Coco1.jpg';
+import coco2 from '../images/Coco2.jpg';
+import coco3 from '../images/Coco3.jpg';
+import coco4 from '../images/Coco4.jpg';
+import coco5 from '../images/Coco5.jpg';
+import coco6 from '../images/Coco6.jpg';
+import coco7 from '../images/Coco7.jpg';
+import coco8 from '../images/Coco8.jpg';
+import coco9 from '../images/Coco9.jpg';
+import coco10 from '../images/Coco10.jpg';
+import coco11 from '../images/Coco11.jpg';
+import coco12 from '../images/Coco12.jpg';
+import coco13 from '../images/Coco13.jpg';
+import coco14 from '../images/Coco14.jpg';
+import coco15 from '../images/Coco15.jpg';
+import coco16 from '../images/Coco16.jpg';
+import coco17 from '../images/Coco17.jpg';
+import coco18 from '../images/Coco18.jpg';
+import coco19 from '../images/Coco19.jpg';
+import coco20 from '../images/Coco20.jpg';
+import coco21 from '../images/Coco21.jpg';
+import coco22 from '../images/Coco22.jpg';
+import coco23 from '../images/Coco23.jpg';
+import coco24 from '../images/Coco24.jpg';
+import coco25 from '../images/Coco25.jpg';
+import coco26 from '../images/Coco26.jpg';
+import coco27 from '../images/Coco27.jpg';
+import coco28 from '../images/Coco28.jpg';
+import coco29 from '../images/Coco29.jpg';
+import coco30 from '../images/Coco30.jpg';
+import coco31 from '../images/Coco31.jpg';
+import coco32 from '../images/Coco32.jpg';
+import Flyn1 from '../images/Flyn1.jpg';
+import Flyn2 from '../images/Flyn2.jpg';
+import Flyn3 from '../images/Flyn3.jpg';
+import Flyn4 from '../images/Flyn4.jpg';
+import Flyn5 from '../images/Flyn5.jpg';
+import Flyn6 from '../images/Flyn6.jpg';
+import Flyn7 from '../images/Flyn7.jpg';
+import Flyn8 from '../images/Flyn8.jpg';
+import Flyn9 from '../images/Flyn9.jpg';
+import Flyn10 from '../images/Flyn10.jpg';
+import Flyn11 from '../images/Flyn11.jpg';
+import Flyn12 from '../images/Flyn12.jpg';
+import Flyn13 from '../images/Flyn13.jpg';
+import Flyn14 from '../images/Flyn14.jpg';
+import Flyn15 from '../images/Flyn15.jpg';
+import Flyn16 from '../images/Flyn16.jpg';
+import Flyn17 from '../images/Flyn17.jpg';
+import Flyn18 from '../images/Flyn18.jpg';
+import Flyn19 from '../images/Flyn19.jpg';
+import Flyn20 from '../images/Flyn20.jpg';
+import Flyn21 from '../images/Flyn21.jpg';
+import Flyn22 from '../images/Flyn22.jpg';
+import Flyn23 from '../images/Flyn23.jpg';
+import Flyn24 from '../images/Flyn24.jpg';
+import Flyn25 from '../images/Flyn25.jpg';
+import Flyn26 from '../images/Flyn26.jpg';
+import Flyn27 from '../images/Flyn27.jpg';
+import Flyn28 from '../images/Flyn28.jpg';
+import Flyn29 from '../images/Flyn29.jpg';
+import Flyn30 from '../images/Flyn30.jpg';
+import Flyn31 from '../images/Flyn31.jpg';
+import Flyn32 from '../images/Flyn32.jpg';
+
+const Photos = [
+  {
+    photoName: 'aavec1',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: true,
+    attribute: ['가구', '언더웨어', '도구없음'],
+    pic: aavec1,
+    thumb: aavec1,
+  },
+  {
+    photoName: 'aavec2',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['가구', '언더웨어', '도구없음'],
+    pic: aavec2,
+    thumb: aavec2,
+  },
+  {
+    photoName: 'aavec3',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: true,
+    attribute: ['가구', '언더웨어', '도구없음'],
+    pic: aavec3,
+    thumb: aavec3,
+  },
+  {
+    photoName: 'aavec4',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['테마', '진한 그림자', '언더웨어', '도구없음'],
+    pic: aavec4,
+    thumb: aavec4,
+  },
+  {
+    photoName: 'aavec5',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: true,
+    attribute: ['테마', '진한 그림자', '언더웨어', '도구없음'],
+    pic: aavec5,
+    thumb: aavec5,
+  },
+  {
+    photoName: 'aavec6',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['테마', '진한 그림자', '언더웨어', '도구없음'],
+    pic: aavec6,
+    thumb: aavec6,
+  },
+  {
+    photoName: 'aavec7',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['테마', '언더웨어', '도구없음'],
+    pic: aavec7,
+    thumb: aavec7,
+  },
+  {
+    photoName: 'aavec8',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['테마', '흑백사진', '언더웨어', '도구없음'],
+    pic: aavec8,
+    thumb: aavec8,
+  },
+  {
+    photoName: 'aavec9',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['테마', '언더웨어', '도구없음'],
+    pic: aavec9,
+    thumb: aavec9,
+  },
+  {
+    photoName: 'aavec10',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '하의', '도구없음'],
+    pic: aavec10,
+    thumb: aavec10,
+  },
+  {
+    photoName: 'aavec11',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: true,
+    attribute: ['가구', '의상없음', '도구없음'],
+    pic: aavec11,
+    thumb: aavec11,
+  },
+  {
+    photoName: 'aavec12',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '하의', '도구없음'],
+    pic: aavec12,
+    thumb: aavec12,
+  },
+  {
+    photoName: 'aavec13',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '기타의상', '도구없음'],
+    pic: aavec13,
+    thumb: aavec13,
+  },
+  {
+    photoName: 'aavec14',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '기타의상', '도구없음'],
+    pic: aavec14,
+    thumb: aavec14,
+  },
+  {
+    photoName: 'aavec15',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '기타의상', '도구없음'],
+    pic: aavec15,
+    thumb: aavec15,
+  },
+  {
+    photoName: 'aavec16',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['흑백사진', '하의', '기타도구', '소가구'],
+    pic: aavec16,
+    thumb: aavec16,
+  },
+  {
+    photoName: 'aavec17',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['흑백사진', '하의', '기타도구'],
+    pic: aavec17,
+    thumb: aavec17,
+  },
+  {
+    photoName: 'aavec18',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['흑백사진', '하의', '기타도구', '소가구'],
+    pic: aavec18,
+    thumb: aavec18,
+  },
+  {
+    photoName: 'aavec19',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['가구', '의상없음', '도구없음'],
+    pic: aavec19,
+    thumb: aavec19,
+  },
+  {
+    photoName: 'aavec20',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['가구', '의상없음', '도구없음'],
+    pic: aavec20,
+    thumb: aavec20,
+  },
+  {
+    photoName: 'aavec21',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: true,
+    attribute: ['가구', '의상없음', '도구없음'],
+    pic: aavec21,
+    thumb: aavec21,
+  },
+  {
+    photoName: 'aavec22',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['가구', '언더웨어', '도구없음'],
+    pic: aavec22,
+    thumb: aavec22,
+  },
+  {
+    photoName: 'aavec23',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['화이트/블랙', '언더웨어', '도구없음'],
+    pic: aavec23,
+    thumb: aavec23,
+  },
+  {
+    photoName: 'aavec24',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['가구', '언더웨어', '도구없음'],
+    pic: aavec24,
+    thumb: aavec24,
+  },
+  {
+    photoName: 'aavec25',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: aavec25,
+    thumb: aavec25,
+  },
+  {
+    photoName: 'aavec26',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: aavec26,
+    thumb: aavec26,
+  },
+
+  {
+    photoName: 'aavec27',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: aavec27,
+    thumb: aavec27,
+  },
+  {
+    photoName: 'aavec28',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: aavec28,
+    thumb: aavec28,
+  },
+  {
+    photoName: 'aavec29',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['테마', '상의', '하의', '도구없음'],
+    pic: aavec29,
+    thumb: aavec29,
+  },
+
+  {
+    photoName: 'aavec30',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: aavec30,
+    thumb: aavec30,
+  },
+
+  {
+    photoName: 'aavec31',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '상의', '하의', '도구없음'],
+    pic: aavec31,
+    thumb: aavec31,
+  },
+
+  {
+    photoName: 'aavec32',
+    studio: 'aavec',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '상의', '하의', '도구없음'],
+    pic: aavec32,
+    thumb: aavec32,
+  },
+  {
+    photoName: 'balanceButton1',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['화이트/블랙', '상의', '도구없음'],
+    pic: balanceButton1,
+    thumb: balanceButton1,
+  },
+  {
+    photoName: 'balanceButton2',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '하의', '기타도구', '소도구'],
+    pic: balanceButton2,
+    thumb: balanceButton2,
+  },
+
+  {
+    photoName: 'balanceButton3',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '도구없음'],
+    pic: balanceButton3,
+    thumb: balanceButton3,
+  },
+  {
+    photoName: 'balanceButton4',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '도구없음'],
+    pic: balanceButton4,
+    thumb: balanceButton4,
+  },
+  {
+    photoName: 'balanceButton5',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '기타도구', '소도구'],
+    pic: balanceButton5,
+    thumb: balanceButton5,
+  },
+  {
+    photoName: 'balanceButton6',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소가구'],
+    pic: balanceButton6,
+    thumb: balanceButton6,
+  },
+
+  {
+    photoName: 'balanceButton7',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '하의', '기타도구', '기타운동도구'],
+    pic: balanceButton7,
+    thumb: balanceButton7,
+  },
+  {
+    photoName: 'balanceButton8',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: balanceButton8,
+    thumb: balanceButton8,
+  },
+
+  {
+    photoName: 'balanceButton9',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '하의', '소가구', '기타도구'],
+    pic: balanceButton9,
+    thumb: balanceButton9,
+  },
+
+  {
+    photoName: 'balanceButton10',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '하의', '기타도구'],
+    pic: balanceButton10,
+    thumb: balanceButton10,
+  },
+  {
+    photoName: 'balanceButton11',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '수영복/레깅스', '도구없음'],
+    pic: balanceButton11,
+    thumb: balanceButton11,
+  },
+
+  {
+    photoName: 'balanceButton12',
+    studio: 'balanceButton',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '상의', '소가구'],
+    pic: balanceButton12,
+    thumb: balanceButton12,
+  },
+  {
+    photoName: 'balanceButton13',
+    studio: 'balanceButton',
+    sex: 'male',
+    horizon: false,
+    attribute: ['컬러', '하의', '소도구', '기타도구', '기타운동도구'],
+    pic: balanceButton13,
+    thumb: balanceButton13,
+  },
+  {
+    photoName: 'balanceButton14',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: balanceButton14,
+    thumb: balanceButton14,
+  },
+  {
+    photoName: 'balanceButton15',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소도구', '기타도구'],
+    pic: balanceButton15,
+    thumb: balanceButton15,
+  },
+  {
+    photoName: 'balanceButton16',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소도구', '기타도구', '기타운동도구'],
+    pic: balanceButton16,
+    thumb: balanceButton16,
+  },
+  {
+    photoName: 'balanceButton17',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소도구', '기타도구'],
+    pic: balanceButton17,
+    thumb: balanceButton17,
+  },
+  {
+    photoName: 'balanceButton18',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소가구'],
+    pic: balanceButton18,
+    thumb: balanceButton18,
+  },
+
+  {
+    photoName: 'balanceButton19',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '도구없음'],
+    pic: balanceButton19,
+    thumb: balanceButton19,
+  },
+
+  {
+    photoName: 'balanceButton20',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '언더웨어', '소가구'],
+    pic: balanceButton20,
+    thumb: balanceButton20,
+  },
+  {
+    photoName: 'balanceButton21',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '기타도구'],
+    pic: balanceButton21,
+    thumb: balanceButton21,
+  },
+  {
+    photoName: 'balanceButton22',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '도구없음'],
+    pic: balanceButton22,
+    thumb: balanceButton22,
+  },
+
+  {
+    photoName: 'balanceButton23',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '기타운동도구', '소도구', '기타도구'],
+    pic: balanceButton23,
+    thumb: balanceButton23,
+  },
+  {
+    photoName: 'balanceButton24',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소가구'],
+    pic: balanceButton24,
+    thumb: balanceButton24,
+  },
+  {
+    photoName: 'balanceButton25',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소도구'],
+    pic: balanceButton25,
+    thumb: balanceButton25,
+  },
+  {
+    photoName: 'balanceButton26',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음'],
+    pic: balanceButton26,
+    thumb: balanceButton26,
+  },
+  {
+    photoName: 'balanceButton27',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '소도구'],
+    pic: balanceButton27,
+    thumb: balanceButton27,
+  },
+  {
+    photoName: 'balanceButton28',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '언더웨어', '도구없음', '기타도구', '기타의상'],
+    pic: balanceButton28,
+    thumb: balanceButton28,
+  },
+  {
+    photoName: 'balanceButton29',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '기타도구', '소도구'],
+    pic: balanceButton29,
+    thumb: balanceButton29,
+  },
+  {
+    photoName: 'balanceButton30',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '기타의상', '소가구'],
+    pic: balanceButton30,
+    thumb: balanceButton30,
+  },
+  {
+    photoName: 'balanceButton31',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '하의', '소가구', '기타도구'],
+    pic: balanceButton31,
+    thumb: balanceButton31,
+  },
+  {
+    photoName: 'balanceButton31',
+    studio: 'balanceButton',
+    sex: 'female',
+    horizon: false,
+    attribute: ['컬러', '상의', '도구없음'],
+    pic: balanceButton31,
+    thumb: balanceButton32,
+  },
+];
