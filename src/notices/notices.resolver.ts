e0f5be71a3fe79539b5771725c4c87a9be6cf523
Woojun Ro <cto@fmonth.com>
2021-01-28 14:34:39 +0900

    Implement createNotice

--- src/notices/notices.resolver.ts
+++ src/notices/notices.resolver.ts
@@ -1,4 +1,22 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Roles } from 'src/auth/roles.decorator';
+import { Role } from 'src/users/entities/user.entity';
+import {
+  CreateNoticeInput,
+  CreateNoticeOutput,
+} from './dtos/create-notice.dto';
+import { Notice } from './entity/notice.entity';
+import { NoticesService } from './notices.service';
 
-@Resolver()
-export class NoticesResolver {}
+@Resolver(of => Notice)
+export class NoticesResolver {
+  constructor(private readonly noticesService: NoticesService) {}
+
+  @Roles(Role.ADMIN)
+  @Mutation(returns => CreateNoticeOutput)
+  createNotice(
+    @Args('input') input: CreateNoticeInput,
+  ): Promise<CreateNoticeOutput> {
+    return this.noticesService.createNotice(input);
+  }
+}
--- src/notices/notices.service.ts
+++ src/notices/notices.service.ts
@@ -1,4 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UNEXPECTED_ERROR } from 'src/common/constants/error.constant';
+import { Repository } from 'typeorm';
+import {
+  CreateNoticeInput,
+  CreateNoticeOutput,
+} from './dtos/create-notice.dto';
+import { Notice } from './entity/notice.entity';
 
 @Injectable()
-export class NoticesService {}
+export class NoticesService {
+  constructor(
+    @InjectRepository(Notice)
+    private readonly noticeRepository: Repository<Notice>,
+  ) {}
+
+  async createNotice({
+    title,
+    content,
+  }: CreateNoticeInput): Promise<CreateNoticeOutput> {
+    try {
+      const newNotice = await this.noticeRepository.save(
+        this.noticeRepository.create({ title, content }),
+      );
+      return { ok: true };
+    } catch (e) {
+      console.log(e);
+      return UNEXPECTED_ERROR;
+    }
+  }
+}
