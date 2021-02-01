8030051ece2c798b462697bb1c32aa944fbdae08
Woojun Ro <cto@fmonth.com>
2021-02-01 12:23:05 +0900

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   const PORT = Number(process.env.PORT) || 8080;
   await app.listen(PORT);
 }
