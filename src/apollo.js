b1545ffd1b6e411a35c0bff743338013124b8f31
Woojun Ro <cto@fmonth.com>
2021-02-03 16:20:47 +0900

    [upload] apollo.js 생성 body: graphql 사용을 위해 apollo client 설치 후 localStorage에 저장된 인증 토큰을 헤더에 붙여 서버와 통신할 수 있도록 설정하였습니다.

---
+++ src/apollo.js
@@ -0,0 +1,21 @@
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'https://api.bodypose.co.kr/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('jwt');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+export const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
