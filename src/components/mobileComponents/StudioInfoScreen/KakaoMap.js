b993cc1dd19effcd54f18df66b133a3d21069ad4
Woojun Ro <cto@fmonth.com>
2021-02-19 18:53:00 +0900

    [refactor] 카카오 지도 API 및 환경변수 리팩토링

--- src/components/mobileComponents/StudioInfoScreen/KakaoMap.js
+++ src/components/mobileComponents/StudioInfoScreen/KakaoMap.js
@@ -5,56 +5,50 @@ import React from 'react';
 class KakaoMap extends React.Component {
   componentDidMount() {
     const { currentLocation } = this.props;
+    const { kakao } = window;
 
-    const script = document.createElement('script');
-    script.async = true;
-    script.src =
-      'https://dapi.kakao.com/v2/maps/sdk.js?appkey=5c06d664dacc5a982b64b8e10bc8f9bb&libraries=LIBRARY';
-    document.head.appendChild(script);
+    kakao.maps.load(() => {
+      let container = document.getElementById('map');
+      //위도,경도, 줌 정도
+      let options = {
+        center: new kakao.maps.LatLng(0, 0),
+        level: 5,
+      };
 
-    script.onload = () => {
-      kakao.maps.load(() => {
-        let container = document.getElementById('map');
-        //위도,경도, 줌 정도
-        let options = {
-          center: new kakao.maps.LatLng(0, 0),
-          level: 5,
-        };
+      const map = new window.kakao.maps.Map(container, options);
+      var bounds = new kakao.maps.LatLngBounds();
 
-        const map = new window.kakao.maps.Map(container, options);
-        var bounds = new kakao.maps.LatLngBounds();
+      // 주소-좌표 변환 객체를 생성합니다
+      var geocoder = new kakao.maps.services.Geocoder();
 
-        // 주소-좌표 변환 객체를 생성합니다
-        var geocoder = new kakao.maps.services.Geocoder();
+      currentLocation.forEach(adr => {
+        // 주소로 좌표를 검색합니다
+        geocoder.addressSearch(adr.address, function (result, status) {
+          // 정상적으로 검색이 완료됐으면
+          if (status === kakao.maps.services.Status.OK) {
+            var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
 
-        currentLocation.forEach(adr => {
-          // 주소로 좌표를 검색합니다
-          geocoder.addressSearch(adr.address, function (result, status) {
-            // 정상적으로 검색이 완료됐으면
-            if (status === kakao.maps.services.Status.OK) {
-              var coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+            // 결과값으로 받은 위치를 마커로 표시합니다
+            var marker = new kakao.maps.Marker({
+              map: map,
+              position: coords,
+            });
 
-              // 결과값으로 받은 위치를 마커로 표시합니다
-              var marker = new kakao.maps.Marker({
-                map: map,
-                position: coords,
+            // 인포윈도우로 장소에 대한 설명을 표시합니다
+            if (adr.title) {
+              var infowindow = new kakao.maps.InfoWindow({
+                content: `<div style="width:150px;text-align:center;padding:2px 0;">${adr.title}</div>`,
               });
-
-              // 인포윈도우로 장소에 대한 설명을 표시합니다
-              if (adr.title) {
-                var infowindow = new kakao.maps.InfoWindow({
-                  content: `<div style="width:150px;text-align:center;padding:2px 0;">${adr.title}</div>`,
-                });
-                infowindow.open(map, marker);
-              }
-              bounds.extend(coords);
-              map.setBounds(bounds);
+              infowindow.open(map, marker);
             }
-          });
+            bounds.extend(coords);
+            map.setBounds(bounds);
+          }
         });
       });
-    };
+    });
   }
+
   render() {
     return (
       <>
