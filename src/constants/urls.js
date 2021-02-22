a3a4bdecb3c61c6df7436f37776868e1a49ded0a
Woojun Ro <cto@fmonth.com>
2021-02-22 18:59:37 +0900

    [fix] Fix HOMESCREEN_AD_URL

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -4,4 +4,4 @@ const isDevUrl = false;
 export const BASE_URL = isDevUrl ? DEV_SERVER_URL : PROD_SERVER_URL;
 
 export const HOMESCREEN_AD_URL =
-  'https://storage.googleapis.com/bodypose-storage/ads/big%20door%20ads.png';
+  'https://storage.googleapis.com/bodypose-storage/ads/big%20door.png';
