97641688521f1f7dbcebf847b7893cd36a277672
Woojun Ro <cto@fmonth.com>
2021-03-11 22:37:06 +0900

    [refactor] 스튜디오 커버 사진 수정 가능하도록 코드 변경

--- src/studios/dtos/update-studio.dto.ts
+++ src/studios/dtos/update-studio.dto.ts
@@ -24,6 +24,7 @@ class UpdateStudioPayload extends PartialType(
       'weekdayPriceTag',
       'weekendPriceTag',
       'additionalProductListDescription',
+      'coverPhotoUrl',
     ],
     InputType,
   ),
