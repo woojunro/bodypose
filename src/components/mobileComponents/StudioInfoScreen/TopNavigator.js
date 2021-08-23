230e301e074fccf84be68df123398ef0dd43c7d3
재승 김 <ceo@fmonth.com>
2021-08-23 15:28:43 +0900

    navigator change complete

--- src/components/mobileComponents/StudioInfoScreen/TopNavigator.js
+++ src/components/mobileComponents/StudioInfoScreen/TopNavigator.js
@@ -3,49 +3,29 @@ import './TopNavigator.css';
 
 const TopNavigator = ({ navigator, setNavigator, reviews = 0 }) => {
   const items = [
-    { name: 'item', title: '상품' },
     { name: 'portfolio', title: '포트폴리오' },
+    { name: 'item', title: '상품' },
     { name: 'info', title: '정보' },
     // { name: 'review', title: '리뷰' },
   ];
 
   const renderedItems = items.map(item => {
     if (navigator === item.name) {
-      if (item.name === 'review') {
-        return (
-          <div key={item.title} className="selectedItem">
-            <span>{item.title}</span>
-          </div>
-        );
-      } else {
-        return (
-          <div key={item.title} className="selectedItem">
-            {item.title}
-          </div>
-        );
-      }
+      return (
+        <div key={item.title} className="selectedItem">
+          <div className="selectedTitle">{item.title}</div>
+        </div>
+      );
     } else {
-      if (item.name === 'review') {
-        return (
-          <div
-            key={item.title}
-            onClick={() => setNavigator(item.name)}
-            className="unselectedItem"
-          >
-            <span>{item.title}</span>
-          </div>
-        );
-      } else {
-        return (
-          <div
-            key={item.title}
-            onClick={() => setNavigator(item.name)}
-            className="unselectedItem"
-          >
-            {item.title}
-          </div>
-        );
-      }
+      return (
+        <div
+          key={item.title}
+          onClick={() => setNavigator(item.name)}
+          className="unselectedItem"
+        >
+          {item.title}
+        </div>
+      );
     }
   });
 
