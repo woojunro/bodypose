48ab9b756c35975f3459688d923c06a221a0133c
재승 김 <ceo@fmonth.com>
2021-09-24 15:33:58 +0900

    footer fixed

--- src/components/mobileComponents/Footer.js
+++ src/components/mobileComponents/Footer.js
@@ -4,7 +4,7 @@ import { PRIVACY_NOTICE_ID, TEMRS_NOTICE_ID } from '../../constants/noticeIds';
 import './Footer.css';
 
 const Footer = () => {
-  const openInNewTab = (url) => {
+  const openInNewTab = url => {
     const newWindow = window.open(url, '_blank', 'noopener,noreferrer');
     if (newWindow) newWindow.opener = null;
   };
@@ -48,12 +48,41 @@ const Footer = () => {
           입점문의
         </div>
       </div>
-      <div
-        className="footerText"
-        style={{ marginTop: '5px' }}
-        onClick={() => openInNewTab('mailTo:help@fmonth.com')}
-      >
-        메일문의 help@fmonth.com
+      <div className="footerInfo">
+        <div className="footerLeftContainer">
+          <img
+            src="https://storage.googleapis.com/bodypose-storage/hompage/fmonth_logo.png"
+            alt="logo"
+            className="footerLogo"
+            onClick={() => openInNewTab('http://fmonth.com')}
+          />
+          <div>에프먼스</div>
+          <div>대표 : 김재승</div>
+          <div className="contacts">
+            <div className="contactText">Contact :</div>
+            <div
+              className="footerContactLink"
+              onClick={() => {
+                window.open('https://pf.kakao.com/_xbxoHEs', '_blank').focus();
+              }}
+            >
+              카카오채널
+            </div>
+            <div className="contactDivider">|</div>
+            <div
+              className="footerContactLink"
+              onClick={() => {
+                window.open('mailTo:help@fmonth.com').focus();
+              }}
+            >
+              help@fmonth.com
+            </div>
+          </div>
+        </div>
+        <div className="footerRightContainer">
+          <div>MON ~ FRI</div>
+          <div>09:30 ~ 19:00</div>
+        </div>
       </div>
     </div>
   );
